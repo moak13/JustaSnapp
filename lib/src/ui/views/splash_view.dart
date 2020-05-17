0942afb0828d226ef5603a9b978f218f12f68cd2
moak13 <michaelkalango@gmail.com>
2020-05-17 14:26:44 +0100

    splash view in progress

--- lib/src/ui/views/splash_view.dart
+++ lib/src/ui/views/splash_view.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //backgroundColor: Colors.grey,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/bg_img.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 110,
+                  left: 86,
+                  right: 86,
+                  bottom: 2.5,
+                ),
+                child: Text(
+                  'JustaSnapp',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      color: Color(0XFF303C42),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 2.5,
+                  left: 115,
+                  right: 115,
+                  bottom: 465,
+                ),
+                child: Text(
+                  "Everyone's Maerket",
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: Color(0XFF303C42),
+                    ),
+                  ),
+                ),
+              )
+            ]),
+      ),
+    );
   }
 }

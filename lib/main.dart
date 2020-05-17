7122d28bf917d69e73ec997cfe2766b01c24853f
moak13 <michaelkalango@gmail.com>
2020-05-17 13:26:20 +0100

    starter project structure

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:justasnapp/src/ui/views/splash_view.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JustaSnapp',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashView()
+    );
+  }
+}

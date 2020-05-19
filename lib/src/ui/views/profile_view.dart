ec89b2f94dde49dda01ed2387a0c310d8b49502b
Emamezi <emamezi.ebebeinwe@stu.cu.edu.ng>
2020-05-19 21:27:42 +0100

    profile view updated

--- lib/src/ui/views/profile_view.dart
+++ lib/src/ui/views/profile_view.dart
@@ -8,12 +8,74 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
-
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('Profile View'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: deviceSize.height * 0.2,
+            child: Center(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Icon(Icons.person, color: Colors.white),
+                  backgroundColor: Colors.grey,
+                ),
+                title: Text('User Name'),
+                subtitle: Text(
+                  'User Email',
+                ),
+              ),
+            ),
+          ),
+          DefaultTabController(
+            length: 2,
+            child: Expanded(
+              child: SizedBox(
+                // height: 200,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: deviceSize.height * 0.1,
+                      width: double.infinity,
+                      color: Color(0xffededed),
+                      child: TabBar(
+                        labelColor: Theme.of(context).primaryColor,
+                        indicatorColor: Theme.of(context).primaryColor,
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        indicatorPadding: EdgeInsets.symmetric(horizontal: 30),
+                        tabs: [
+                          Text('Selling',),
+                          Text('Sold'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: TabBarView(
+                        children: [
+                          Container(
+                            // color: Colors.green,
+                            child: Center(
+                                child: Text(
+                                    'You don\'t have any item on Sale Yet')),
+                          ),
+                          Container(
+                            // color: Colors.blue,
+                            child: Center(
+                                child: Text('You have not sold any item yet')),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

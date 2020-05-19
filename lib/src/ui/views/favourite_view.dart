a03c41f2c9f6788cb81af0d3391f7b9acc3d993b
moak13 <michaelkalango@gmail.com>
2020-05-19 23:25:17 +0100

    favorite no item view done

--- lib/src/ui/views/favourite_view.dart
+++ lib/src/ui/views/favourite_view.dart
@@ -12,9 +12,15 @@ class _FavouriteViewState extends State<FavouriteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Favourite View'),
-      ),
+      body:Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text('You have no ads in your favorite'),
+          ),
+          Expanded(child: Container())
+        ],
+      )
     );
   }
 }

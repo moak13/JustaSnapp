c812f53737a1741845de516e29d9bedcc28995c0
Emamezi <emamezi.ebebeinwe@stu.cu.edu.ng>
2020-05-22 16:36:16 +0100

    about JustaSnapp view done

--- lib/src/ui/views/aboutJustaSnapp_view.dart
+++ lib/src/ui/views/aboutJustaSnapp_view.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AboutJustaSnappView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: null,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('About JustaSnapp'),
+        leading: IconButton(
+          splashColor: Colors.transparent,
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 70,
+                left: 86,
+                right: 86,
+                bottom: 2.5,
+              ),
+              child: Text(
+                'JustaSnapp',
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: ScreenUtil().setSp(35),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "Everyone's Market",
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: ScreenUtil().setSp(16),
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+              ),
+              child: Text('Contact us: support@justasnapp.ng'),
+            )
+          ]),
     );
   }
 }
